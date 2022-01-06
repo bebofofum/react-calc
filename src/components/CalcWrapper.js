7ab06da76734e18828db54e84d9dd1a5048ba6c6
Brian Boehm <bri.boehm@gmail.com>
2022-01-05 19:29:18 -0500

    Clean up files and make outer wrapper compoent

---
+++ src/components/CalcWrapper.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CalcWrapper = ({children}) => {
+    return (
+        <div className="outer-wrapper">I'll be the wrapper</div>
+    )
+}
+
+export default CalcWrapper
