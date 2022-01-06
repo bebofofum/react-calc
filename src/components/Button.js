a45b50daf9e2ff0e8f04c22e3208b993a0e2575a
Brian Boehm <bri.boehm@gmail.com>
2022-01-06 13:09:23 -0500

    Creating button component with styles. Added buttons to ButtonWrapper. Will refactor later.

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = (props) => {
+    console.log([props]);
+
+    return(
+        <button >
+            A button Value
+        </button>
+    )
+}
--- src/components/ButtonWrapper.js
+++ src/components/ButtonWrapper.js
@@ -4,7 +4,22 @@ import '../styles/ButtonWrapper.css';
 const ButtonWrapper = ({children}) => {
     return (
         <div className="button-container">
-            <p>I'll be the button wrapper</p>
+            <button className="" value="0" onClick={() => console.log("clicked!")}>0</button>
+            <button className="" value="0" onClick={() => console.log("clicked!")}>1</button>
+            <button className="" value="0" onClick={() => console.log("clicked!")}>2</button>
+            <button className="" value="0" onClick={() => console.log("clicked!")}>3</button>
+            <button className="" value="0" onClick={() => console.log("clicked!")}>4</button>
+            <button className="" value="0" onClick={() => console.log("clicked!")}>&#61;</button>
+            <button className="" value="0" onClick={() => console.log("clicked!")}>5</button>
+            <button className="" value="0" onClick={() => console.log("clicked!")}>6</button>
+            <button className="" value="0" onClick={() => console.log("clicked!")}>7</button>
+            <button className="" value="0" onClick={() => console.log("clicked!")}>8</button>
+            <button className="" value="0" onClick={() => console.log("clicked!")}>9</button>
+            <button className="" value="0" onClick={() => console.log("clicked!")}>&#43;</button>
+            <button className="" value="0" onClick={() => console.log("clicked!")}>&minus;</button>
+            <button className="" value="0" onClick={() => console.log("clicked!")}>&times;</button>
+            <button className="" value="0" onClick={() => console.log("clicked!")}>&divide;</button>
+
         </div>
     )
 }
