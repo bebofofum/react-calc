e3cc2dd0b7853c392f40fad1dd9d9a032db520e6
Brian Boehm <bri.boehm@gmail.com>
2022-01-08 19:09:38 -0500

    Added function to receive value from event target and display that value in console. Making sure button clicked knows value of button.

--- src/components/ButtonWrapper.js
+++ src/components/ButtonWrapper.js
@@ -2,30 +2,33 @@ import React from 'react';
 import '../styles/ButtonWrapper.css';
 import '../styles/Button.css';
 
-const btnValue = [0, 1, 2, 3, 4, "&#61;", 5, 6, 7, 8, 9, "&#43;", "&minus;", "&times;", "&divide;"]
+// const btnValue = [0, 1, 2, 3, 4, "&#61;", 5, 6, 7, 8, 9, "&#43;", "&minus;", "&times;", "&divide;"]
 
+const addButtonDetail = (e) => {
+    console.log("clicked", e.target.value)
+}
 
 
 const ButtonWrapper = ({children}) => {
 
     return (
         <div className="button-container">
-            <button className="" value="0" onClick={() => console.log()}>0</button>
-            <button className="" value="1" onClick={() => console.log("clicked!")}>1</button>
-            <button className="" value="2" onClick={() => console.log("clicked!")}>2</button>
-            <button className="" value="3" onClick={() => console.log("clicked!")}>3</button>
-            <button className="" value="4" onClick={() => console.log("clicked!")}>4</button>
-            <button className="equals-button" value="0" onClick={() => console.log("clicked!")}>&#61;</button>
-            <button className="" value="5" onClick={() => console.log("clicked!")}>5</button>
-            <button className="" value="6" onClick={() => console.log("clicked!")}>6</button>
-            <button className="" value="7" onClick={() => console.log("clicked!")}>7</button>
-            <button className="" value="8" onClick={() => console.log("clicked!")}>8</button>
-            <button className="" value="9" onClick={() => console.log("clicked!")}>9</button>
-            <button className="" value="0" onClick={() => console.log("clicked!")}>&#43;</button>
-            <button className="" value="0" onClick={() => console.log("clicked!")}>&minus;</button>
-            <button className="" value="0" onClick={() => console.log("clicked!")}>&times;</button>
-            <button className="" value="0" onClick={() => console.log("clicked!")}>&divide;</button>
-            <button className="clear-button" value="0" onClick={() => console.log("clicked!")}>C</button>
+            <button className="" value={0} onClick={(e) => {addButtonDetail(e, "value")}}>0</button>
+            <button className="" value={1} onClick={(e) => {addButtonDetail(e, "value")}}>1</button>
+            <button className="" value={2} onClick={(e) => {addButtonDetail(e, "value")}}>2</button>
+            <button className="" value={3} onClick={(e) => {addButtonDetail(e, "value")}}>3</button>
+            <button className="" value={4} onClick={(e) => {addButtonDetail(e, "value")}}>4</button>
+            <button className="equals-button" value={"="} onClick={(e) => {addButtonDetail(e, "value")}}>&#61;</button>
+            <button className="" value={5} onClick={(e) => {addButtonDetail(e, "value")}}>5</button>
+            <button className="" value={6} onClick={(e) => {addButtonDetail(e, "value")}}>6</button>
+            <button className="" value={7} onClick={(e) => {addButtonDetail(e, "value")}}>7</button>
+            <button className="" value={8} onClick={(e) => {addButtonDetail(e, "value")}}>8</button>
+            <button className="" value={9} onClick={(e) => {addButtonDetail(e, "value")}}>9</button>
+            <button className="" value={"+"} onClick={(e) => {addButtonDetail(e, "value")}}>&#43;</button>
+            <button className="" value="-" onClick={(e) => {addButtonDetail(e, "value")}}>&minus;</button>
+            <button className="" value="X" onClick={(e) => {addButtonDetail(e, "value")}}>&times;</button>
+            <button className="" value="/" onClick={(e) => {addButtonDetail(e, "value")}}>&divide;</button>
+            <button className="clear-button" value={"C"} onClick={(e) => {addButtonDetail(e, "value")}}>C</button>
         </div>
     )
 }
